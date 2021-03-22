3c40fdbd9be8ce9585dad39cd3e9c716901034c7
Douglas Basilio dos Santos <basilio.douglas@yahoo.com.br>
2021-03-22 08:48:37 -0300

    POO C#

---
+++ 02-ByteBank/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = "Douglas";
+            Console.WriteLine(conta.titular);
+
+            conta.saldo = 200.0;
+
+            Console.WriteLine(conta.agencia);
+            Console.WriteLine(conta.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
