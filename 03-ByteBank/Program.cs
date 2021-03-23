3b9929611c0fe66a97ffecb32170d73000fd731c
Douglas Basilio dos Santos <basilio.douglas@yahoo.com.br>
2021-03-23 08:27:58 -0300

    Criando método Sacar()

---
+++ 03-ByteBank/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente c1 = new ContaCorrente();
+
+            c1.titular = "Douglas";
+            Console.WriteLine("Saldo anterior: " + c1.saldo);
+            c1.Sacar(50);
+
+            Console.WriteLine("Saldo após saque: " + c1.saldo);
+            Console.ReadLine();
+        }
+    }
+}
