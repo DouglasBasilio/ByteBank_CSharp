18244b3311bf86c158904ff36853f9f7a10ca3ac
Douglas Basilio dos Santos <basilio.douglas@yahoo.com.br>
2021-03-24 08:31:29 -0300

    Referência a um objeto e adicionando namespace às classes

---
+++ 04-ByteBank/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente douglas = new Cliente();
+
+            douglas.nome = "Douglas";
+            douglas.profissao = "Desenvolvedor C#";
+            douglas.cpf = "111.111.111-20";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = douglas;
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.numero = 5634527;
+
+            conta.titular.nome = "Douglas Basilio";
+
+            Console.WriteLine(conta.titular.nome);
+
+            Console.ReadLine();
+        }
+    }
+}
